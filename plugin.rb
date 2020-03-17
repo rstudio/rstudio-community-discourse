70689948926c7c0e4c721959d1dcc84727791424
Angus McLeod <angus@mcleod.org.au>
2020-03-17 11:12:06 +1100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -22,6 +22,7 @@ after_initialize do
     add_to_class(:topic, field.to_sym) { self.custom_fields[field.to_s] }
     add_to_serializer(:topic_view, field.to_sym) { object.topic.send(field.to_s) }
     add_to_serializer(:topic_list_item, field.to_sym) { object.send(field.to_s) }
+    TopicList.preloaded_custom_fields << field.to_s
     
     PostRevisor.track_topic_field(field.to_sym) do |tc, val|
       tc.record_change(field.to_s, tc.topic.custom_fields[field.to_s], val)
